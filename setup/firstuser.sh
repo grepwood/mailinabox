b32218c98e97e8fbf58bcfb5eb5c166fb16baa27
grepwood <grepwood@sucs.org>
2014-12-04 14:48:13 +0000

    it's -z tests again

--- setup/firstuser.sh
+++ setup/firstuser.sh
@@ -11,8 +11,9 @@ if [ -z "`tools/mail.py user`" ]; then
 				"Let's create your first mail account.
 				\n\nWhat email address do you want?" \
 				me@`get_default_hostname` \
-				$EMAIL_ADDR
-
+				EMAIL_ADDR
+			
+			echo "$EMAIL_ADDR"
 			if [ "$EMAIL_ADDR" == " " ]; then
 				# user hit ESC/cancel
 				exit
