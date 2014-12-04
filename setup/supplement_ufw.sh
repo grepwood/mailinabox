f9be9c55e3c41cea95a2f176c55318671a32b4e6
grepwood <grepwood@sucs.org>
2014-12-04 11:24:12 +0000

    forgot tar line

--- setup/supplement_ufw.sh
+++ setup/supplement_ufw.sh
@@ -4,6 +4,7 @@
 LATEST_UFW=`curl https://launchpad.net/ufw 2>/dev/null | grep Latest\ version\ is | awk '{print $4}'`
 cd /tmp
 curl -LO https://launchpad.net/ufw/$LATEST_UFW/$LATEST_UFW/+download/ufw-$LATEST_UFW.tar.gz 2>/dev/null
+tar -xf ufw-$LATEST_UFW.tar.gz
 cd ufw-$LATEST_UFW
 yum install iptables-ipv6 -y -q
 python setup.py install
