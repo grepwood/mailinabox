d36e2fc826180e3fe100054e7bd884b58679eded
grepwood <grepwood@sucs.org>
2014-12-05 10:32:11 +0000

    ubuntu is being a git

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -9,35 +9,6 @@
 DISTRO=""
 SATISFIED=0
 
-function prepare_crossdistro {
-	if command -v 2>&1 1>/dev/null; then
-		DISTRO="RedHat"
-		SATISFIED=1
-		if [ "`rpm -qa redhat-lsb | wc -l`" -eq "0" ]; then
-			yum install redhat-lsb -y -q
-		fi
-	elif command -v apt-get 2>&1 1>/dev/null; then
-		DISTRO="Ubuntu"
-		SATISFIED=1
-	else
-		echo "Your distro is not supported"
-		exit
-	fi
-}
-
-function detect_distro {
-	if command -v 2>&1 1>/dev/null; then
-		DISTRO="RedHat"
-		SATISFIED=1
-	elif command -v apt-get 2>&1 1>/dev/null; then
-		DISTRO="Ubuntu"
-		SATISFIED=1
-	else
-		echo "Your distro is not supported"
-		exit
-	fi
-}
-
 if [ -z "$TAG" ]; then
 	TAG="v0.05"
 fi
@@ -48,7 +19,20 @@ if [ "`whoami`" != "root" ]; then
 	exit
 fi
 
-prepare_crossdistro
+if command -v 2>&1 1>/dev/null; then
+	DISTRO="RedHat"
+	SATISFIED=1
+	if [ "`rpm -qa redhat-lsb | wc -l`" -eq "0" ]; then
+		SHUT_UP_UBUNTU=yum install redhat-lsb -y -q
+	fi
+elif command -v apt-get 2>&1 1>/dev/null; then
+	DISTRO="Ubuntu"
+	SATISFIED=1
+else
+	echo "Your distro is not supported"
+	exit
+fi
+
 if [ "$DISTRO" == "Ubuntu" ]; then
 	apt-get -q -q install -y git < /dev/null
 elif [ "$DISTRO" == "RedHat" ]; then
