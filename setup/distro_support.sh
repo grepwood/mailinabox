57b1add9d1e2b69a0f5ee2a980bbb7469b509bae
grepwood <grepwood@sucs.org>
2014-12-08 15:18:47 +0000

    distro detect now offers python divergence

--- setup/distro_support.sh
+++ setup/distro_support.sh
@@ -1,6 +1,7 @@
 DISTRO=""
 DISTRO_VERSION=0
 SATISFIED=0
+PYTHON=""
 
 function prepare_crossdistro {
 	if command -v yum 2>&1 1>/dev/null; then
@@ -12,11 +13,21 @@ function prepare_crossdistro {
 	elif command -v apt-get 2>&1 1>/dev/null; then
 		DISTRO="Ubuntu"
 		SATISFIED=1
+		PYTHON="python3"
 	else
 		echo "Your distro is not supported"
 		exit
 	fi
 	DISTRO_VERSION=`lsb_release -r | awk '{print $2}' | sed -e 's:\.:\ :g' | awk '{print $1$2}'`
+	if [ "$DISTRO" = "Ubuntu" ]; then
+		PYTHON="python3"
+	elif [ "$DISTRO" = "RedHat" ]; then
+		if [ "$DISTRO_VERSION" -ge "70" ]; then
+			PYTHON="python3"
+		elif [ "$DISTRO_VERSION" -lt "70" ]; then
+			PYTHON="python2"
+		fi
+	fi
 }
 
 function detect_distro {
@@ -31,4 +42,13 @@ function detect_distro {
 		exit
 	fi
 	DISTRO_VERSION=`lsb_release -r | awk '{print $2}' | sed -e 's:\.:\ :g' | awk '{print $1$2}'`
+	if [ "$DISTRO" = "Ubuntu" ]; then
+		PYTHON="python3"
+	elif [ "$DISTRO" = "RedHat" ]; then
+		if [ "$DISTRO_VERSION" -ge "70" ]; then
+			PYTHON="python3"
+		elif [ "$DISTRO_VERSION" -lt "70" ]; then
+			PYTHON="python2"
+		fi
+	fi
 }
