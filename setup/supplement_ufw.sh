5076b22428ef829adaf8c286b129c36357d8dc41
grepwood <grepwood@sucs.org>
2014-12-18 11:54:28 +0000

    stops the stdout spam

--- setup/supplement_ufw.sh
+++ setup/supplement_ufw.sh
@@ -7,8 +7,10 @@ cd /tmp
 curl -LO https://launchpad.net/ufw/$LATEST_UFW/$LATEST_UFW/+download/ufw-$LATEST_UFW.tar.gz 2>/dev/null
 tar -xf ufw-$LATEST_UFW.tar.gz
 cd ufw-$LATEST_UFW
-yum install iptables-ipv6 -y -q
-$PYTHON setup.py install
+if [ "`rpm -qa iptables-ipv6 | wc -l`" -eq "0" ]; then
+	yum install iptables-ipv6 -y -q
+fi
+$PYTHON setup.py install 2>&1 1>/dev/null
 cd $REAL_PWD
 rm -rf /tmp/ufw-$LATEST_UFW{,.tar.gz}
 chmod 644 /etc/default/ufw /etc/ufw/ufw.conf /etc/ufw/applications.d/ufw-*
